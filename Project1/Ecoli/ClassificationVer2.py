f670747b02cbd36887868396c310459a44328f61
TomAbrahams <UTKeitarol@gmail.com>
2017-03-10 02:23:23 -0800

    Update ClassificationVer2.py
    Changed to 'softmax' instead of logarithmic. It is getting around 88%

--- Project1/Ecoli/ClassificationVer2.py
+++ Project1/Ecoli/ClassificationVer2.py
@@ -72,27 +72,33 @@ import numpy as np
 
 with open("F:\\ecoli\\ecoli_proc.data") as f:
     ncols = len(f.readline().split())
-print(ncols)
-print('\n')
+#print(ncols)
+#print('\n')
 ecoli = np.loadtxt('ecoli_proc.data',dtype='S', usecols = range(1,ncols)).astype(np.float)
-print(ecoli.shape)
-print(ecoli)
+#print(ecoli.shape)
+#print(ecoli)
 ecoli[:,:7] = ecoli[:,:7]-ecoli[:,:7].mean(axis=0)
 imax = np.concatenate((ecoli.max(axis=0)*np.ones((1,8)),np.abs(ecoli.min(axis=0)*np.ones((1,8)))),axis=0).max(axis=0)
-print("imax size")
-print(imax.shape)
-print('\n')
-print(ecoli[:,:8].shape)
-print('\n')
+#print("imax size")
+#print(imax.shape)
+#print('\n')
+#print(ecoli[:,:7].shape)
+#print('\n')
 ecoli[:,:7] = ecoli[:,:7]/imax[:7]
 #dataset[:,:n-1] = dataset[:,:n-1]-dataset[:,:n-1].mean(axis=0)
 #imax= np.concatenate((dataset.max(axis=0)*np.ones((1,n-1))), np.abs(dataset.min(axis=0)*np.ones((1,n-1))),axis=0).max(axis=0)
 #dataset[:,:n-1] = dataset[:,:n-1]/imax[:n-1]
-print ("Here comes some values\n")
-print ecoli[0:5,:]
+#print ("Here comes some values\n")
+print ecoli[0:8,:]
+
 
 # Split into training, validation, and test sets
+
+#print("The np.shape(iris)[0]")
+#print(np.shape(ecoli)[0])
 target = np.zeros((np.shape(ecoli)[0],8));
+                 
+print(np.shape(ecoli)[0],8)
 indices = np.where(ecoli[:,7]==0) 
 target[indices,0] = 1
 indices = np.where(ecoli[:,7]==1)
@@ -109,46 +115,52 @@ indices = np.where(ecoli[:,7]==6)
 target[indices,6] = 1
 indices = np.where(ecoli[:,7]==7)
 target[indices,7] = 1
-      
+#print("Indices")
+#print(indices)
+#print("target")
+#print(target)
+#print(target.size)
 # Randomly order the data
 order = range(np.shape(ecoli)[0])
 np.random.shuffle(order)
 
 ecoli = ecoli[order,:]
 target = target[order,:]
-print("Ecoli \n")
-print(ecoli)
+#print("Ecoli \n")
+#print(ecoli)
 
-train = ecoli[::2,0:7] # Want every second element, from colums 0,1,2,3
-print("Shape of train is ")
-print(train.shape)
-print('\n')
-traint = target[::2]    # Want to target every second element period.
-print("Shape of traint is ")
-print(traint.shape)
-print('\n')
-valid = ecoli[1::4,0:7]  # Starting from row 1 get me the forth element, from columns 0,1,2,3 so the 5th item
-print("output of valid is: \n")
-print(valid.shape)
-print("\nShape of valid is ")
-print(valid.shape)
-print('\n')
-validt = target[1::4]   # From row 1, get me every 4th element. this has 84 elements so the 5th item
-print("Shape of validt is ")
-print(validt.shape)
-print('\n')
-test = ecoli[3::4,0:7]   # From the third row on, get me every 4th element in columns 0,1,2,3 so the 7th item
-print("Shape of test is ")
-print(test.shape)
-print('\n')
-testt = target[3::4]    # From the third row on, get me every 4th element 7th.
-print("Shape of testt is ")
-print(testt.shape)
-print('\n')
+train = ecoli[0:168,0:7] # Want every second element, from colums 0,1,2,3
+#print("Shape of train is ")
+#print(train.shape)
+#print('\n')
+traint = target[0:168]    # Want to target every second element period.
+#print("Shape of traint is ")
+#print(traint.shape)
+#print('\n')
+valid = ecoli[168:252,0:7]  # Starting from row 1 get me the forth element, from columns 0,1,2,3 so the 5th item
+#print("output of valid is: \n")
+#print(valid.shape)
+#print("\nShape of valid is ")
+#print(valid.shape)
+#print('\n')
+validt = target[168:252]   # From row 1, get me every 4th element. this has 84 elements so the 5th item
+#print("Shape of validt is ")
+#print(validt.shape)
+#print('\n')
+test = ecoli[252:336, 0:7]   # From the third row on, get me every 4th element in columns 0,1,2,3 so the 7th item
+#print("Shape of test is ")
+#print(test.shape)
+#print('\n')
+testt = target[252:336]    # From the third row on, get me every 4th element 7th.
+#print("Shape of testt is ")
+#print(testt.shape)
+#print('\n')
 #print train.max(axis=0), train.min(axis=0)
 
 # Train the network
 import mlp
-net = mlp.mlp(train,traint,60,outtype='logistic')
+net = mlp.mlp(train,traint,8,outtype='softmax')
 net.earlystopping(train,traint,valid,validt,0.1)
 net.confmat(test,testt)
+#print train.max(axis=0), train.min(axis=0)
+
